8d093c0c999d8e2e26182b26d670f7763e33c7f8
mudigonda <mayur.mudigonda@gmail.com>
2016-06-17 16:44:59 -0700

    Pushing scaffold for the LSTM code. still need to push the main function

--- rnnlhc/fitting/simple_lstm.py
+++ rnnlhc/fitting/simple_lstm.py
@@ -1,15 +1,170 @@
 import tensorflow as tf
-from utilities import parse_data
-from utilities import plot_data
+from rnnlhc.fitting.utilities import parse_data
+from rnnlhc.fitting import BatchData
+import argparse
+'''
+Class 
 
-#Load Data
+'''
 
-#Define Cell
-lstm = tf.nn.rnn_cell.BasicLSTMCell(size,forget_bias=0.0)
+class rnnlhc(object):
+    """ The RNN LHC Model with just simple number of hidden weights """
+    def __init__(self,is_training,config):
+        
+#Trelf.batch_size = batch_size = config.batch_size
+    self.num_steps = num_steps = config.num_steps
+    size = config.hidden_size
+    vocab_size = config.vocab_size
 
-#Create Batch Data
+    self._input_data = tf.placeholder(tf.int32, [batch_size, num_steps])
+    self._targets = tf.placeholder(tf.int32, [batch_size, num_steps])
 
-#Define truncated backprop
+    # Slightly better results can be obtained with forget gate biases
+    # initialized to 1 but the hyperparameters of the model would need to be
+    # different than reported in the paper.
+    lstm_cell = tf.nn.rnn_cell.BasicLSTMCell(size, forget_bias=0.0)
+    if is_training and config.keep_prob < 1:
+      lstm_cell = tf.nn.rnn_cell.DropoutWrapper(
+          lstm_cell, output_keep_prob=config.keep_prob)
+    cell = tf.nn.rnn_cell.MultiRNNCell([lstm_cell] * config.num_layers)
 
-#Train Model
+    self._initial_state = cell.zero_state(batch_size, tf.float32)
+
+    with tf.device("/gpu:0"):
+      embedding = tf.get_variable("embedding", [vocab_size, size])
+      inputs = tf.nn.embedding_lookup(embedding, self._input_data)
+
+    if is_training and config.keep_prob < 1:
+      inputs = tf.nn.dropout(inputs, config.keep_prob)
+
+    # Simplified version of tensorflow.models.rnn.rnn.py's rnn().
+    # This builds an unrolled LSTM for tutorial purposes only.
+    # In general, use the rnn() or state_saving_rnn() from rnn.py.
+    #
+    # The alternative version of the code below is:
+    #
+    # from tensorflow.models.rnn import rnn
+    # inputs = [tf.squeeze(input_, [1])
+    #           for input_ in tf.split(1, num_steps, inputs)]
+    # outputs, state = rnn.rnn(cell, inputs, initial_state=self._initial_state)
+    outputs = []
+    state = self._initial_state
+    with tf.variable_scope("RNN"):
+      for time_step in range(num_steps):
+        if time_step > 0: tf.get_variable_scope().reuse_variables()
+        (cell_output, state) = cell(inputs[:, time_step, :], state)
+        outputs.append(cell_output)
+
+    output = tf.reshape(tf.concat(1, outputs), [-1, size])
+    softmax_w = tf.get_variable("softmax_w", [size, vocab_size])
+    softmax_b = tf.get_variable("softmax_b", [vocab_size])
+    logits = tf.matmul(output, softmax_w) + softmax_b
+    loss = tf.nn.seq2seq.sequence_loss_by_example(
+        [logits],
+        [tf.reshape(self._targets, [-1])],
+        [tf.ones([batch_size * num_steps])])
+    self._cost = cost = tf.reduce_sum(loss) / batch_size
+    self._final_state = state
+
+    if not is_training:
+      return
+
+    self._lr = tf.Variable(0.0, trainable=False)
+    tvars = tf.trainable_variables()
+    grads, _ = tf.clip_by_global_norm(tf.gradients(cost, tvars),
+                                      config.max_grad_norm)
+    optimizer = tf.train.GradientDescentOptimizer(self.lr)
+    self._train_op = optimizer.apply_gradients(zip(grads, tvars))
+
+  def assign_lr(self, session, lr_value):
+    session.run(tf.assign(self.lr, lr_value))
+
+  @property
+  def input_data(self):
+    return self._input_data
+
+  @property
+  def targets(self):
+    return self._targets
+
+  @property
+  def initial_state(self):
+    return self._initial_state
+
+  @property
+  def cost(self):
+    return self._cost
+
+  @property
+  def final_state(self):
+    return self._final_state
+
+  @property
+  def lr(self):
+    return self._lr
+
+  @property
+  def train_op(self):
+    return self._train_op
+
+class Config(object):
+  """Small config."""
+  init_scale = 0.1
+  learning_rate = 1.0
+  max_grad_norm = 5
+  num_layers = 2
+  num_steps = 20
+  hidden_size = 200
+  max_epoch = 4
+  max_max_epoch = 13
+  keep_prob = 1.0
+  lr_decay = 0.5
+  batch_size = 20
+  vocab_size = 10000
+
+class TestConfig(object):
+  """Tiny config, for testing."""
+  init_scale = 0.1
+  learning_rate = 1.0
+  max_grad_norm = 1
+  num_layers = 1
+  num_steps = 2
+  hidden_size = 2
+  max_epoch = 1
+  max_max_epoch = 1
+  keep_prob = 1.0
+  lr_decay = 0.5
+  batch_size = 20
+  vocab_size = 10000
+
+
+def run_model():
+  """Runs the model on the given data."""
+  epoch_size = ((len(data) // m.batch_size) - 1) // m.num_steps
+  start_time = time.time()
+  costs = 0.0
+  iters = 0
+  state = m.initial_state.eval()
+  for step, (x, y) in enumerate(reader.ptb_iterator(data, m.batch_size,
+                                                    m.num_steps)):
+    cost, state, _ = session.run([m.cost, m.final_state, eval_op],
+                                 {m.input_data: x,
+                                  m.targets: y,
+                                  m.initial_state: state})
+    costs += cost
+    iters += m.num_steps
+
+    if verbose and step % (epoch_size // 10) == 10:
+      print("%.3f perplexity: %.3f speed: %.0f wps" %
+            (step * 1.0 / epoch_size, np.exp(costs / iters),
+             iters * m.batch_size / (time.time() - start_time)))
+
+  return np.exp(costs / iters)
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description='Parser to provide flags for the LSTM')
+    parser.add_argument('--checkpoint',type=str,default="/media/Gondor/Data/rnnlhc/")
+    args = parser.parse_args()
+    json_data = parse_data(fname='../data/EventDump_10tracks.json')
 
