8c2f9f59cf2dde80393ebfaabe9e6de99e997e43
mudigonda <mayur.mudigonda@gmail.com>
2016-07-31 00:05:13 -0700

    Trying to add eval op as well. Stuck with a specific issue on non iteratable tensor

--- rnnlhc/fitting/simpler_lstm.py
+++ rnnlhc/fitting/simpler_lstm.py
@@ -10,40 +10,58 @@ class testrnn:
     def __init__(self,config):
         self.config = config
         tf.reset_default_graph()
-        self.input_data = tf.placeholder(tf.float32,[None,config.MaxNumSteps])
-        self.target = tf.placeholder(tf.float32,[None,config.MaxNumSteps])
-        loss = tf.Variable(0.,trainable=False)
-        x_split = tf.split(0,config.batch_size,self.input_data)
-        y_split = tf.split(0,config.batch_size,self.target)
+        with tf.variable_scope("rnnlch") as scope:
+            self.input_data = tf.placeholder(tf.float32,[None,config.MaxNumSteps])
+            self.eval_input_data = tf.placeholder(tf.float32,[1,3])#create eval node, 3 time steps
+            self.eval_target = tf.Variable(tf.constant(0.0,shape=[SEQ_LENGTH]))
+            self.target = tf.placeholder(tf.float32,[None,config.MaxNumSteps])
+            loss = tf.Variable(0.,trainable=False)
+            x_split = tf.split(0,config.batch_size,self.input_data)
+            y_split = tf.split(0,config.batch_size,self.target)
+            x_eval_split = tf.split(0,1,self.eval_input_data)# split the evaluation input by the number of time steps
 
-        w = tf.Variable(tf.random_normal([config.hidden_size,config.FC_Units],stddev=0.1),trainable=True)
-        b = tf.Variable(tf.constant(0.0,shape=[config.FC_Units]),trainable=True)
-        w_2 = tf.Variable(tf.random_normal([config.FC_Units,config.MaxNumSteps],stddev=0.1),trainable=True)
-        b_2 = tf.Variable(tf.constant(0.0,shape=[config.MaxNumSteps]),trainable=True)
-        #Initialize basic lstm cell
-        lstm = tf.nn.rnn_cell.BasicLSTMCell(config.hidden_size,state_is_tuple=True)
-        ops, states = tf.nn.rnn(lstm,x_split,dtype=tf.float32)
-        #lstm_multi = tf.nn.rnn_cell.MultiRNNCell([lstm]*config.num_layers,state_is_tuple=True)
-        #ops, states = tf.nn.rnn(lstm_multi,x_split,dtype=tf.float32)
-        #Compute loss
-        for op,target in zip(ops,y_split):
-            transform = tf.nn.elu(tf.matmul(op,w)+b)
-            drop_out = tf.nn.dropout(transform,keep_prob=0.6)
-            fc_layer2 = tf.nn.elu(tf.matmul(drop_out,w_2)+b_2)
-            loss += self.loss_function(transform,target,w,b)
+            w = tf.Variable(tf.random_normal([config.hidden_size,config.FC_Units],stddev=0.1),trainable=True)
+            b = tf.Variable(tf.constant(0.0,shape=[config.FC_Units]),trainable=True)
+            w_2 = tf.Variable(tf.random_normal([config.FC_Units,config.MaxNumSteps],stddev=0.1),trainable=True)
+            b_2 = tf.Variable(tf.constant(0.0,shape=[config.MaxNumSteps]),trainable=True)
+            #Initialize basic lstm cell
+            lstm = tf.nn.rnn_cell.BasicLSTMCell(config.hidden_size,state_is_tuple=True)
+            ops, states = tf.nn.rnn(lstm,x_split,dtype=tf.float32)
+            #lstm_multi = tf.nn.rnn_cell.MultiRNNCell([lstm]*config.num_layers,state_is_tuple=True)
+            #ops, states = tf.nn.rnn(lstm_multi,x_split,dtype=tf.float32)
+            self.output = []
+            #Compute loss
+            for op,target in zip(ops,y_split):
+                transform = tf.nn.elu(tf.matmul(op,w)+b)
+                drop_out = tf.nn.dropout(transform,keep_prob=0.6)
+                fc_layer2 = tf.nn.elu(tf.matmul(drop_out,w_2)+b_2)
+                self.output.append(fc_layer2)
+                loss += self.loss_function(transform,target,w,b)
 
-        self.loss = loss
-        self.lr = tf.Variable(0.0, trainable=False)
-        '''
-        tvars = tf.trainable_variables()
-        grads, _ = tf.clip_by_global_norm(tf.gradients(loss, tvars),
-                                      config.max_grad_norm)
-        #optimizer = tf.train.GradientDescentOptimizer(self.lr)
-        #optimizer = tf.train.MomentumOptimizer(learning_rate=self.lr,momentum=0.5)
-        optimizer = tf.train.AdamOptimizer(learning_rate=self.lr)
-        self.train_op = optimizer.apply_gradients(zip(grads, tvars))
-        '''
-        self.train_op = tf.train.AdamOptimizer(learning_rate=self.lr).minimize(loss)
+            #Use the variables above to also unravel the eval node
+            self.loss = loss
+            self.lr = tf.Variable(0.0, trainable=False)
+            '''
+            tvars = tf.trainable_variables()
+            grads, _ = tf.clip_by_global_norm(tf.gradients(loss, tvars),
+                                          config.max_grad_norm)
+            #optimizer = tf.train.GradientDescentOptimizer(self.lr)
+            #optimizer = tf.train.MomentumOptimizer(learning_rate=self.lr,momentum=0.5)
+            optimizer = tf.train.AdamOptimizer(learning_rate=self.lr)
+            self.train_op = optimizer.apply_gradients(zip(grads, tvars))
+            '''
+            self.train_op = tf.train.AdamOptimizer(learning_rate=self.lr).minimize(loss)
+            #Eval network
+            scope.reuse_variables()
+            for tstep in range(SEQ_LENGTH):
+                if tstep < 3:
+                    import IPython; IPython.embed()
+                    tmp = lstm(x_eval_split,tf.zeros([1,1]))
+                    self.eval_target[tstep] = tmp
+                else:
+                    self.eval_target[tstep] = lstm(self.eval_target[tstep])
+
+
 
 
 
@@ -107,14 +125,14 @@ if __name__ == "__main__":
    cost_lst = []
    with tf.Session() as sess:
         sess.run(tf.initialize_all_variables())
-        for ii in range(5000):
+        for ii in range(500):
             m.assign_lr(sess,config.learning_rate)
             ind,data = m.generate_data(m.config.batch_size)
             cost = run_model(sess,m,data,m.train_op)
             cost_lst.append(cost)
             if np.mod(ii,100) == 0:
                 print("cost is {}".format(cost))
-   plt.plot(cost_lst)
-   plt.title('Cost vs iterations')
-   plt.savefig('RNN_train_1.png')
-   import IPython; IPython.embed()
+        plt.plot(cost_lst)
+        plt.title('Cost vs iterations')
+        plt.savefig('RNN_train_1.png')
+        import IPython; IPython.embed()
