ec4410d245f6fb412f7a315869d3afe31c5927bd
Steve Farrell <sfarrell@cern.ch>
2017-02-12 11:07:34 -0800

    Added BiLSTM model to models module

--- discrete_extraps/models.py
+++ discrete_extraps/models.py
@@ -4,7 +4,7 @@ This file contains code to construct keras models.
 
 # External imports
 from keras.models import Model
-from keras.layers import Input, LSTM, Dense, TimeDistributed
+from keras.layers import Input, LSTM, Dense, TimeDistributed, Bidirectional
 from keras.regularizers import l2
 
 def SeqDense(name=None, *args, **kwargs):
@@ -16,7 +16,7 @@ def build_lstm_model(length, dim, hidden_dim=100,
                      optimizer='Nadam', metrics=['accuracy']):
     """
     Build the simple LSTM model.
-    
+
     This is a sequence to sequence model with the following
     architecture:
         Input -> LSTM -> Dense -> Output
@@ -36,7 +36,7 @@ def build_deep_lstm_model(length, dim, hidden_dim=100,
                           metrics=['accuracy']):
     """
     Build the deep LSTM model.
-    
+
     This is a sequence to sequence model with the following architecture:
         Input -> Dense -> LSTM -> Dense -> Dense -> Output
     Input and output data must have shape:
@@ -51,6 +51,29 @@ def build_deep_lstm_model(length, dim, hidden_dim=100,
         Dense(hidden_dim, activation='relu', W_regularizer=l2(l2reg)))(hidden2)
     outputs = TimeDistributed(
         Dense(dim, activation='softmax', W_regularizer=l2(l2reg)))(hidden3)
+    model = Model(input=inputs, output=outputs)
+    model.compile(loss=loss, optimizer=optimizer, metrics=metrics)
+    return model
+
+def build_bilstm_model(length, dim, hidden_dim=100,
+                       loss='categorical_crossentropy',
+                       optimizer='Nadam', metrics=['accuracy']):
+    """
+    Build the bidirectional LSTM model.
+
+    This is a sequence to sequence model with the following
+    architecture:
+        Input -> BiLSTM -> Dense -> Output
+    Input and output data must have shape:
+        (num_batch, length, dim).
+    """
+    inputs = Input(shape=(length, dim))
+    hidden = Bidirectional(
+        LSTM(hidden_dim, return_sequences=True))(inputs)
+    hidden = TimeDistributed(
+        Dense(hidden_dim, activation='relu'))(hidden)
+    outputs = TimeDistributed(
+        Dense(dim, activation='softmax'))(hidden)
     model = Model(input=inputs, output=outputs)
     model.compile(loss=loss, optimizer=optimizer, metrics=metrics)
     return model
