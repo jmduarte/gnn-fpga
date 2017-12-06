dca0cf607086c7182e3b967e578886bd7ed87a68
Steve Farrell <sfarrell@cern.ch>
2017-12-06 09:45:26 -0800

    adding more functions to ease data loading

--- tree_search/data.py
+++ tree_search/data.py
@@ -6,7 +6,10 @@ The code in this module depends on pandas.
 from __future__ import print_function
 
 import ast
+import multiprocessing as mp
+
 import pandas as pd
+import numpy as np
 
 def load_data_events(file_name, columns, start_evtid=0):
     """
@@ -39,3 +42,29 @@ def load_data_events(file_name, columns, start_evtid=0):
     
     # Concatenate the events together into one DataFrame
     return pd.concat(dfs, ignore_index=True)
+
+def process_hits_data(df, copy_keys=['evtid', 'barcode', 'volid', 'layid']):
+    """Split columns and calculate some derived variables"""
+    x = df.gpos.apply(lambda pos: pos[0])
+    y = df.gpos.apply(lambda pos: pos[1])
+    z = df.gpos.apply(lambda pos: pos[2])
+    r = np.sqrt(x**2 + y**2)
+    phi = np.arctan2(y, x)
+    return df[copy_keys].assign(z=z.astype(np.float32), r=r.astype(np.float32),
+                                phi=phi.astype(np.float32))
+
+def read_worker(hits_file):
+    hits_columns = ['hitid', 'barcode', 'volid', 'layid', 'lpos',
+                    'lerr', 'gpos', 'chans', 'dir', 'direrr']
+    return process_hits_data(load_data_events(hits_file, columns=hits_columns))
+
+def process_files(hits_files, num_workers):
+    """Load and process a set of hits files with MP"""
+    pool = mp.Pool(processes=num_workers)
+    hits = pool.map(read_worker, hits_files)
+    pool.close()
+    pool.join()
+    # Fix the evtid to be consecutive
+    for i in range(1, len(hits)):
+        hits[i].evtid += hits[i-1].evtid.iloc[-1] + 1
+    return pd.concat(hits, ignore_index=True)
