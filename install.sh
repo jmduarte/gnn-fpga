dac0b94b1d0ed18f27375f152996e1b348f17472
Javier Duarte <jduarte1@FNAL.GOV>
2018-05-22 17:54:03 -0500

    setup on rulinux

---
+++ install.sh
@@ -0,0 +1,4 @@
+conda create --copy --name pytorch-training python=3.6
+conda install --name pytorch-training --file pytorch-training.conda
+source activate pytorch-training
+pip install sklearn scipy
