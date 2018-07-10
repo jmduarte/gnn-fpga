745b67b2742acb76002effe7d1a8165573e306c1
Javier Duarte <jduarte1@fnal.gov>
2018-07-10 19:17:28 +0000

    update to use GPU

--- install.sh
+++ install.sh
@@ -1,4 +1,7 @@
 conda create --copy --name pytorch-training python=3.6
+# for gpu:
+#conda install --name pytorch-training --file pytorch-training-gpu.conda
+# for cpu:
 conda install --name pytorch-training --file pytorch-training.conda
 source activate pytorch-training
 pip install sklearn scipy
